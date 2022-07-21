e4a477733e1c95df9b7f9faea4a5ba0ef308c6b0
Anominity <anominity@gmail.com>
2022-07-21 09:16:34 +0100

    update

--- week_05/day_3/week5day3hw/app.R
+++ week_05/day_3/week5day3hw/app.R
@@ -12,7 +12,13 @@ cattle_choices <- cattle %>%
   arrange(breed) %>% 
   pull()
 
-
+cattle_full <- cattle %>% 
+  group_by(breed, birth_month) %>% 
+  mutate(total_births = sum(number_of_births)) %>% 
+  ungroup() %>% 
+  select(-country, -gov_region, -birth_year, -application_type) %>% 
+  group_by(breed, birth_month, breed_type, sex, total_births) %>% 
+  distinct(breed)
 
 
 ui <- fluidPage(
@@ -50,16 +56,60 @@ ui <- fluidPage(
   )
 )
 
+# ui <- fluidPage(
+#   titlePanel(
+#     h1("Scottish cattle births in 2015", align = "center")),
+#   br(),
+#   theme = bs_theme(bootswatch = "flatly"),
+#   fluidRow(
+#     column(
+#       width = 12,
+#       plotOutput(
+#         "cattle_plot"
+#       )
+#     )
+#   ),
+#   br(),
+#   fluidRow(
+#     column(
+#       width = 3,
+#       offset = 3,
+#       radioButtons(
+#         inputId = "breed_type",
+#         label = "Non Dairy or Dairy",
+#         choices = c("Non Dairy", "Dairy")
+#       )
+#     ),
+#     column(
+#       width = 6,
+#       selectInput(
+#         inputId = "breed",
+#         label = "Choose Breed",
+#         choices = NULL
+#       )
+#     )
+#   )
+# )
+# 
+# server <- function(input, output, session) {
+#   breed_type <- reactive({
+#     filter(cattle_full, breed == input$breed_type)
+#   })
+#   observeEvent(breed_type(), {
+#     choices <- unique(breed()$breed_type)
+#     updateSelectInput(inputId = "breed", choices = choices) 
+#   })
+
 server <- function(input, output, session) {
   output$cattle_plot <- renderPlot(
-    cattle %>% 
+    cattle_full %>% 
       filter(
         breed_type == input$breed_type,
         breed == input$breed
       ) %>% 
       ggplot() +
-      aes(x = birth_month, fill = sex) +
-      geom_bar(position = "dodge") +
+      aes(x = birth_month, y = total_births, fill = sex) +
+      geom_col(scale = "identity", position = "dodge") +
       xlab(" ") +
       ylab(" ") +
       scale_fill_manual(values = c("M" = "#0096c7", 
@@ -67,7 +117,6 @@ server <- function(input, output, session) {
       scale_x_discrete(limits = month.name) +
       theme(panel.background = element_rect(
         fill = 'white', colour = 'white'))
-    
   )
 }
 
